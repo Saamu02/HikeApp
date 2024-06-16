8268dffd15f114516e5a1f2015f864953ecc0008
Saamu02 <iusamairfan@gmail.com>
2024-06-16 12:08:55 +0500

    Add assets for the project

--- Hike/ContentView.swift
+++ Hike/ContentView.swift
@@ -9,13 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Image("image-3")
+            .resizable()
+            .scaledToFit()
     }
 }
 
