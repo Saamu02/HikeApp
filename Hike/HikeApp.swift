fcd6e2d1f695388a3675337cdb0d81adfdf3bf23
Saamu02 <iusamairfan@gmail.com>
2024-06-16 11:57:59 +0500

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Ussama Irfan on 16/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
